d1880db7aa306cc101c3de3c0ba262c121518813
shrinishLT <shrinishv@lambdatest.com>
2024-10-21 19:59:05 +0530

    fix script to make correct builds for applitools

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ SMART_IGNORE_STAGE_URL = 'https://stage-smartui-screenshot-comparison.lambdatest
 
 
 BASE_DIFF_URL = f'https://stage-automation-dotlapse-artefact.lambdatestinternal.com/dummy-orgId/dummy-projId/dummy-buildId/compared-screenshots/baseDiff1.png'
-COMP_DIFF_URL = 'https://stage-automation-dotlapse-artefact.lambdatestinternal.com/dummy-orgId/dummy-projId/dummy-buildId/compared-screenshots/compDiff1.png'
+COMP_DIFF_URL = f'https://stage-automation-dotlapse-artefact.lambdatestinternal.com/dummy-orgId/dummy-projId/dummy-buildId/compared-screenshots/compDiff1.png'
 
 
 APPLITOOLS_API_KEY = '4BEljunWcWd5PewqoExSEHs2ZSjscnqZqV1bazKu1tY110'
